0b17c50097c0b975c8caf1ca8b126baabf229d5c
Diane Crame <dcrame@icloud.com>
2020-03-24 18:59:25 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ numbers = [1,2,3]
 
 def square_array(numbers)
   newArray = []
-  numbers.each { |number| print newArray.push(number * number,","}
+  numbers.each { |number| print newArray.push(number * number,",")}
 end
 
 square_array(numbers)
